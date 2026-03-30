a9cf8a53933b0a290bfed99436c74fb8832a875e
Conner Smith <basketballbob2003@gmail.com>
2026-03-30 16:42:22 -0400

    Removed line wrapping, added line numbers, added emmet plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,70 +1,71 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        opts = {},
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "ts_ls",
-                "jsonls",
-                "pyright",
-            },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.config('lua.ls', {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' },
-                        },
-                    },
-                },
-            })
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+				"jsonls",
+				"pyright",
+			},
+			automatic_installation = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("lua.ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-            vim.lsp.config('jsonls', {
-                settings = {},
-            })
+			vim.lsp.config("jsonls", {
+				settings = {},
+			})
 
-            vim.api.nvim_create_autocmd('LspAttach', {
-                callback = function(event)
-                    local map = function(keys, func, desc)
-                        vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-                    end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
 
-                    -- Navigation 
-                    local tb = require('telescope.builtin')
-                    map('gd', tb.lsp_definitions, 'Go to Definition')
-                    -- map('gd', vim.lsp.buf.definition, 'Go to Definition')
-                    map('gi', tb.lsp_implementations, 'Go to Implementation')
-                    -- map('gi', vim.lsp.buf.implementation, 'Go to Implementation')
-                    map('gr', tb.lsp_references, 'Go to References')
-                    -- map('gr', vim.lsp.buf.references, 'Go to References')
-                    map('gy', tb.lsp_type_definitions, 'Go to Type Definition')
-                    -- map('gy', vim.lsp.buf.definition, 'Go to Type Definition')
-                    map('gD', vim.lsp.buf.declaration, 'Go to Declaration')
+					-- Navigation
+					local tb = require("telescope.builtin")
+					map("gd", tb.lsp_definitions, "Go to Definition")
+					-- map('gd', vim.lsp.buf.definition, 'Go to Definition')
+					map("gi", tb.lsp_implementations, "Go to Implementation")
+					-- map('gi', vim.lsp.buf.implementation, 'Go to Implementation')
+					map("gr", tb.lsp_references, "Go to References")
+					-- map('gr', vim.lsp.buf.references, 'Go to References')
+					map("gy", tb.lsp_type_definitions, "Go to Type Definition")
+					-- map('gy', vim.lsp.buf.definition, 'Go to Type Definition')
+					map("gD", vim.lsp.buf.declaration, "Go to Declaration")
 
-                    -- Docs / Info
-                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
-                    map('gk', vim.lsp.buf.signature_help, 'Signature Help')
+					-- Docs / Info
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					map("gk", vim.lsp.buf.signature_help, "Signature Help")
 
-                    -- Actions
-                    map('<leader>rn', vim.lsp.buf.rename, 'Rename Symbol')
-                    map('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
-                    map('<leader>f', function()
-                        vim.lsp.buf.format({ async = true})
-                    end, 'Format buffer')
-               end
-            })
-        end
-    }
+					-- Actions
+					map("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+					map("<leader>f", function()
+						vim.lsp.buf.format({ async = true })
+					end, "Format buffer")
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,16 @@ return {
 				-- require("none-ls.diagnostics.eslint"),
 			},
 		})
-
+        
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+        vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, {})
+        vim.keymap.set("n", "<leader>gs", vim.lsp.buf.signature_help, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+        vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
 	end,
 }
