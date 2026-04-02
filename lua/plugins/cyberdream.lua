bff3069c48461cdd172b81f9864dca8e7e9079d4
Conner Smith <basketballbob2003@gmail.com>
2026-04-02 13:37:42 -0400

    Changed modes for neotree; Updated cyberdream config

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -3,6 +3,16 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("cyberdream").setup {
+                transparent = true,
+                styles = {
+                    comments = "italic",
+                    functions = "bold",
+                    keywords = "bold,italic",
+                    variables = "NONE",
+                },
+            }
+
             vim.cmd.colorscheme "cyberdream"
         end
     }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
             "MunifTanjim/nui.nvim"
         },
         config = function()
-            vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-            vim.keymap.set('n', '<C-m>', ':Neotree close<CR>')
+            vim.keymap.set({'n', 'i', 'v'}, '<C-n>', ':Neotree filesystem reveal left<CR>')
+            vim.keymap.set({'n', 'i', 'v'}, '<C-m>', ':Neotree close<CR>')
         end
 }
